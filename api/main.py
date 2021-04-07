37bcc27f7e9114ad0666132982d4ee11317dd19a
Jakub Ruchel <jruchel@gmail.com>
2021-04-07 21:41:54 +0200

    Added an endpoint that allows the user to train a linear regression algorithm on the given data in a csv file, and read the results

---
+++ api/main.py
@@ -0,0 +1,40 @@
+from flask import Flask, json, request
+from flask_cors import CORS, cross_origin
+from sklearn.linear_model import LinearRegression
+from topics.linear_regression import train
+import traceback
+import logging
+import api
+
+
+class LinearRegressionTrainingResult:
+    def __init__(self, accuracy, intercept, coefficients):
+        self.accuracy = accuracy
+        self.intercept = intercept
+        self.coefficients = coefficients
+
+
+api = Flask(__name__)
+cors = CORS(api)
+api.config['CORS_HEADERS'] = 'Content-Type'
+
+
+@api.route('/algorithms/linear-regression', methods=['GET', 'POST'])
+@cross_origin()
+def linear_regression():
+    try:
+        arguments = request.args
+        separator = arguments.get('separator')
+        predicting = arguments.get('predicting')
+        file = request.files['data']
+        model, accuracy = train(file, separator, predicting, LinearRegression())
+        intercept = model.intercept_
+        coefficients = model.coef_
+        return json.dumps(LinearRegressionTrainingResult(accuracy, intercept, coefficients.tolist()).__dict__)
+    except Exception as error:
+        return str(error)
+
+
+if __name__ == "__main__":
+    api.run(host='0.0.0.0')
+    input("Press enter to exit")
